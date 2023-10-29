f9337606db47a8970c6c9a5159b2fe1f69ae583d
Gian <130838241+KRiMZ0001@users.noreply.github.com>
2023-10-30 00:11:16 +0800

    Update GameWindowDrawing.cs

--- External.Farlight84/Drawing/GameWindowDrawing.cs
+++ External.Farlight84/Drawing/GameWindowDrawing.cs
@@ -1,73 +1,80 @@
-﻿using External.Farlight84.Memory;
+using System.Drawing;
 using GameOverlay.Drawing;
 using GameOverlay.Windows;
+using Microsoft.Extensions.Logging;
 
 namespace External.Farlight84.Drawing
 {
-    internal class GameWindowDrawing : IGameWindowDrawing
+    public interface IGameWindowDrawing
     {
-        public static float Height;
-        public static float Width;
+        void Initialize();
+        void DrawText(float x, float y, string text);
+        void DrawLine(float x, float y, float endX, float endY, float stroke);
+        void DrawBox(Rectangle rectangle, float stroke);
+        void DrawProgessBar(Rectangle rectangle, float stroke, float percentage);
+        void BeginScene();
+        void EndScene();
+    }
 
-        private Graphics _graphics = default!;
-        private Font _defaultFont = default!;
-        private SolidBrush _defaultWhiteBrush = default!;
-        private SolidBrush _defaultGreenBrush = default!;
-        private SolidBrush _defaultInvisibleBrush = default!;
-        
+    public class GameWindowDrawing : IGameWindowDrawing
+    {
+        private readonly ILogger<GameWindowDrawing> _logger;
+        private readonly MemoryService _memoryService;
+        private readonly Graphics _graphics;
+        private readonly GameWindowDrawingConfig _config;
+
+        public GameWindowDrawing(ILogger<GameWindowDrawing> logger, MemoryService memoryService, Graphics graphics, GameWindowDrawingConfig config)
+        {
+            _logger = logger;
+            _memoryService = memoryService;
+            _graphics = graphics;
+            _config = config;
+        }
+
         public void Initialize()
         {
-            _graphics = new Graphics(MemoryService.MainWindowHandle)
-            {
-                MeasureFPS = true,
-                PerPrimitiveAntiAliasing = true,
-                TextAntiAliasing = true,
-                VSync = true,
-                UseMultiThreadedFactories = true
-            };
+            _graphics.MeasureFPS = true;
+            _graphics.PerPrimitiveAntiAliasing = true;
+            _graphics.TextAntiAliasing = true;
+            _graphics.VSync = true;
+            _graphics.UseMultiThreadedFactories = true;
 
-            var window = new StickyWindow(MemoryService.MainWindowHandle, _graphics)
+            var window = new StickyWindow(_memoryService.MainWindowHandle, _graphics)
             {
                 IsTopmost = true,
                 IsVisible = true,
-                FPS = 120
+                FPS = _config.FPS
             };
 
             window.Create();
             _graphics.WindowHandle = window.Handle;
             _graphics.Setup();
 
-            _defaultWhiteBrush = _graphics.CreateSolidBrush(255, 255, 255);
-            _defaultInvisibleBrush = _graphics.CreateSolidBrush(0, 0, 0, 0);
-            _defaultGreenBrush = _graphics.CreateSolidBrush(0, 255, 0);
-            _defaultFont = _graphics.CreateFont("Futura", 12);
+            _logger.LogInformation("Initialized GameWindowDrawing");
         }
 
         public void DrawText(float x, float y, string text)
         {
-            _graphics.DrawText(_defaultFont, _defaultWhiteBrush, x, y, text);
+            _graphics.DrawText(_graphics.CreateFont(_config.FontName, _config.FontSize), _graphics.CreateSolidBrush(_config.TextColor), x, y, text);
         }
 
         public void DrawLine(float x, float y, float endX, float endY, float stroke)
         {
-            _graphics.DrawLine(_defaultWhiteBrush, x, y, endX, endY, stroke);
+            _graphics.DrawLine(_graphics.CreateSolidBrush(_config.LineColor), x, y, endX, endY, stroke);
         }
 
         public void DrawBox(Rectangle rectangle, float stroke)
         {
-            _graphics.DrawBox2D(_defaultWhiteBrush, _defaultInvisibleBrush, rectangle, stroke);
+            _graphics.DrawBox2D(_graphics.CreateSolidBrush(_config.BoxColor), _graphics.CreateSolidBrush(_config.BoxFillColor), rectangle, stroke);
         }
 
         public void DrawProgessBar(Rectangle rectangle, float stroke, float percentage)
         {
-            _graphics.DrawHorizontalProgressBar(_defaultWhiteBrush, _defaultGreenBrush, rectangle, stroke, percentage);
+            _graphics.DrawHorizontalProgressBar(_graphics.CreateSolidBrush(_config.ProgressBarColor), _graphics.CreateSolidBrush(_config.ProgressBarFillColor), rectangle, stroke, percentage);
         }
 
         public void BeginScene()
         {
-            Width = _graphics.Width;
-            Height = _graphics.Height;
-
             _graphics.BeginScene();
             _graphics.ClearScene();
         }
@@ -76,5 +83,18 @@ namespace External.Farlight84.Drawing
         {
             _graphics.EndScene();
         }
+    }
+
+    public class GameWindowDrawingConfig
+    {
+        public string FontName { get; set; } = "Futura";
+        public int FontSize { get; set; } = 12;
+        public Color TextColor { get; set; } = Color.White;
+        public Color LineColor { get; set; } = Color.White;
+        public Color BoxColor { get; set; } = Color.White;
+        public Color BoxFillColor { get; set; } = Color.Transparent;
+        public Color ProgressBarColor { get; set; } = Color.White;
+        public Color ProgressBarFillColor { get; set; } = Color.Green;
+        public int FPS { get; set; } = 120;
     }
 }
